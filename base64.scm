40f06bfa16c4bcd52901d0e7ee80dbe1df31418d
WammKD <jaft.r@outlook.com>
2018-05-14 18:06:58 -0500

    Guess I don't need the ASCII values, anyway, so back to Strings they go and set up the `fold` mechanism for the beef of the function.

--- base64.scm
+++ base64.scm
@@ -1,15 +1,21 @@
 (use-modules (rnrs bytevectors) (srfi srfi-1))
 
-(define BASE_64_CHARS (string->utf8 (string-append
-                                      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdef"
-                                      "ghijklmnopqrstuvwxyz0123456789+/")))
+(define BASE_64_CHARS (string-append
+                        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdef"
+                        "ghijklmnopqrstuvwxyz0123456789+/"))
 (define TOKEN_LENGTH  3)
 
 (define (base64-encode stringToEncode)
   (define stringLength  (string-length stringToEncode))
-
   (define padCount      (modulo stringLength TOKEN_LENGTH))
-  (define encodedString (make-bytevector padCount (char->integer #\nul)))
-  (define paddingString (make-bytevector padCount (char->integer #\=)))
 
-  (iota (ceiling (/ stringLength TOKEN_LENGTH)) 0 TOKEN_LENGTH))
+  (define paddingString (make-string padCount #\=))
+  (define paddedString  (string-append
+                          stringToEncode
+                          (make-string padCount #\nul)))
+
+  (fold
+    (lambda (elem result)
+      result)
+    ""
+    (iota (ceiling (/ stringLength TOKEN_LENGTH)) 0 TOKEN_LENGTH)))
