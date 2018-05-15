1eb6208de108fd64aec2d1ee2f1f4be3e5efb578
WammKD <jaft.r@outlook.com>
2018-05-14 20:54:13 -0500

    It's working. I'm gonna change it to only accept bytevectors (though it'll still return Strings), next.

--- base64.scm
+++ base64.scm
@@ -4,18 +4,57 @@
                         "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdef"
                         "ghijklmnopqrstuvwxyz0123456789+/"))
 (define TOKEN_LENGTH  3)
+(define (string-ref->ASCII str k)
+  (char->integer (string-ref str k)))
+(define (int->base64-char i)
+  (string-ref BASE_64_CHARS (logand i 63)))
 
 (define (base64-encode stringToEncode)
-  (define stringLength  (string-length stringToEncode))
-  (define padCount      (modulo stringLength TOKEN_LENGTH))
+  (define padCount      (modulo
+                          (- TOKEN_LENGTH (modulo
+                                            (string-length stringToEncode)
+                                            TOKEN_LENGTH))
+                          TOKEN_LENGTH))
 
   (define paddingString (make-string padCount #\=))
   (define paddedString  (string-append
                           stringToEncode
                           (make-string padCount #\nul)))
+  (define final (fold
+                  (lambda (index encodedString)
+                    (define result (string-append
+                                     encodedString
+                                     (if (and
+                                           (> index 0)
+                                           (= (modulo
+                                                (* (/ index TOKEN_LENGTH) 4)
+                                                76) 0))
+                                         "\r\n"
+                                       "")))
+                    (define n      (+
+                                     (ash (string-ref->ASCII
+                                            paddedString
+                                            index)            16)
+                                     (ash (string-ref->ASCII
+                                            paddedString
+                                            (1+ index))        8)
+                                     (string-ref->ASCII
+                                       paddedString
+                                       (+ index 2))))
 
-  (fold
-    (lambda (elem result)
-      result)
-    ""
-    (iota (ceiling (/ stringLength TOKEN_LENGTH)) 0 TOKEN_LENGTH)))
+                    (string-append
+                      result
+                      (list->string (list
+                                      (int->base64-char (ash n -18))
+                                      (int->base64-char (ash n -12))
+                                      (int->base64-char (ash n -6))
+                                      (int->base64-char n)))))
+                  ""
+                  (iota
+                    (ceiling (/ (string-length paddedString) TOKEN_LENGTH))
+                    0
+                    TOKEN_LENGTH)))
+
+  (string-append
+    (substring final 0 (- (string-length final) (string-length paddingString)))
+    paddingString))
