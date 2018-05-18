5b639d72282ee576903b678680e109a4aeee3fa1
WammKD <jaft.r@outlook.com>
2018-05-17 20:41:55 -0500

    Move all global functions into the helper function (since they're only used there, anyway) and get decode to work with the helper function. Also, add the newlines and returns necessary for the encode function to the helper function so they get done during the process.

--- base64.scm
+++ base64.scm
@@ -6,14 +6,16 @@
                                         "ghijklmnopqrstuvwxyz0123456789+/"))))
 (define TOKEN_LENGTH  3)
 
-(define (int->base64-char i)
-  (integer->char (list-ref BASE_64_CHARS (logand i 63))))
-(define (base64-char-ref->int str i)
-  (list-index (lambda (int)
-                (= int (char->integer (string-ref str i)))) BASE_64_CHARS))
-(define (&255 i)
-  (logand i 255))
+
 (define (bytevector-map-m-to-l m l orig padLength)
+  (define (&255             i)   (logand i 255))
+  (define (invert           j k) (- (1- k) j))
+  (define (int->base64-char i)
+    (integer->char (list-ref BASE_64_CHARS (logand i 63))))
+  (define (base64-char-ref->int str i)
+    (list-index (lambda (int)
+                  (= int (char->integer (string-ref str i)))) BASE_64_CHARS))
+
   (define   origLength ((if (= m 3) length      string-length)   orig))
   (define resultLength (- (* (ceiling (/ origLength m)) l) padLength))
   (define result       ((if (= m 3) make-string make-bytevector) resultLength))
@@ -22,12 +24,10 @@
   (define logand-fun   (if (= m 3) int->base64-char &255))
   (define    set-fun!  (if (= m 3) string-set!      bytevector-u8-set!))
 
-  (define (invert j k) (- (1- k) j))
-
   (do ([increment 0 (+ increment m)]
        [index     0 (+ index     l)])
       [(= increment origLength)]
-    (when (and (= m 3) (> index 0) (= (modulo (* (/ index 3) 4) 76) 0))
+    (when (and (= m 3) (> index 0) (= (modulo (* (/ increment 3) 4) 76) 0))
       (set! result (string-append
                      (substring result 0     index)
                      "\r\n"
@@ -70,37 +70,21 @@
 
 (define (base64-decode stringToDecode)
   (define stripped (string-filter
-                     (lambda (char) (member
-                                      (char->integer char)
-                                      (cons (char->integer #\=) BASE_64_CHARS)))
+                     (lambda (char) (or (char=? char #\=) (member
+                                                            (char->integer char)
+                                                            BASE_64_CHARS)))
                      stringToDecode))
   (define stripLen (string-length stripped))
-  (define padding  (if (char=? (string-ref stripped (1- stripLen)) #\=)
+
+  (define padding  (if (char=? (string-ref stripped (1- stripLen))  #\=)
                        (if (char=? (string-ref stripped (- stripLen 2)) #\=)
                            "AA"
                          "A")
                      ""))
-  (define pLength  (string-length padding))
+  (define padCount (string-length padding))
+
   (define updated  (string-append
-                     (substring stripped 0 (- stripLen pLength))
+                     (substring stripped 0 (- stripLen padCount))
                      padding))
-  (define uLength  (string-length updated))
-  (define rLength  (- (* (ceiling (/ uLength 4)) TOKEN_LENGTH) pLength))
-  (define result   (make-bytevector rLength))
 
-  (do ([increment 0 (+ increment            4)]
-       [index     0 (+ index     TOKEN_LENGTH)])
-      [(= increment uLength)]
-    (let ([n (+
-               (ash (base64-char-ref->int updated       increment) 18)
-               (ash (base64-char-ref->int updated (1+  increment)) 12)
-               (ash (base64-char-ref->int updated (+ increment 2))  6)
-               (base64-char-ref->int updated (+ increment 3)))])
-      (for-each
-        (lambda (i)
-          (let ([newI (+ index (- 2 i))])
-            (when (< newI rLength)
-              (bytevector-u8-set! result newI (&255 (ash n (* i -8)))))))
-        (list 0 1 2))))
-
-  result)
+  (bytevector-map-m-to-l 4 3 updated padCount))
